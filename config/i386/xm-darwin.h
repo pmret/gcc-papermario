4efda41cde9e2afad61255e9c5634a1df154dcbb
bkimmett <bkimmett@users.noreply.github.com>
2020-12-03 23:14:12 -0800

    Add macOS compiling support (properly this time)

---
+++ config/i386/xm-darwin.h
@@ -0,0 +1,4 @@
+/* Configuration for GCC for Intel i386 or later running macOS as host.  */
+
+#include <alloca.h>   // Guess who doesn't support alloca() without a certain include?
+#include "i386/xm-i386.h"
